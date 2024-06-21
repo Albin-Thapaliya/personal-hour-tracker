5e7bea13f424ec7bb63f62dab197c3a71acc470f
Albin-Thapaliya <wingsbroken04@gmail.com>
2024-06-21 02:02:38 +0200

    Update UserStats.js

--- src/UserStats.js
+++ src/UserStats.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function UserStats({ tickets, hours, ticketsSpent, ticketsPending }) {
+function UserStats({ tickets, hours, ticketsSpent, ticketsPending, hoursPerDay }) {
     return (
         <div>
             <h3>My Stats</h3>
@@ -8,6 +8,7 @@ function UserStats({ tickets, hours, ticketsSpent, ticketsPending }) {
             <p>Total Hours Worked: {hours} hours</p>
             <p>Tickets Pending Approval: {ticketsPending}</p>
             <p>Tickets Spent: {ticketsSpent}</p>
+            <p>Hours Needed per Day: {hoursPerDay.toFixed(2)} hours</p>
         </div>
     );
 }
