97a32a9f80326b46265d1dce6943c39cdd695d80
Albin-Thapaliya <wingsbroken04@gmail.com>
2024-06-22 17:38:28 +0100

    Update LoadingSpinner.js

--- src/LoadingSpinner.js
+++ src/LoadingSpinner.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './LoadingSpinner.css';
 
-function LoadingSpinner() {
-    return <div className="spinner">Loading...</div>;
+function LoadingSpinner({ size, color, message }) {
+    return (
+        <div className="spinner-container">
+            <div
+                className={`spinner spinner-${size}`}
+                style={{ borderColor: `${color} transparent transparent transparent` }}
+                aria-label="Loading"
+            />
+            {message && <div className="spinner-message">{message}</div>}
+        </div>
+    );
 }
+
+LoadingSpinner.propTypes = {
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
+    color: PropTypes.string,
+    message: PropTypes.string,
+};
+
+LoadingSpinner.defaultProps = {
+    size: 'medium',
+    color: '#61dafb',
+    message: 'Loading...',
+};
 
 export default LoadingSpinner;
